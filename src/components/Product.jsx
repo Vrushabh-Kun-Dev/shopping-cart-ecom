148302326e1c77b5aa9c0ef82a1a175208d9b9d3
Vrushabh-Kun-Dev <vrushabh.kun.dev@gmail.com>
2025-07-16 12:20:33 +0530

    UI Desing Completed

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -7,11 +7,25 @@ const Product = () => {
           <div className="row">
             {
                 items.map((product)=>{
-                    return(
-                        <>
-                        
-                        </>
-                    )
+                    return (
+                      <>
+                        <div className="col-lg-4 my-3 text-center">
+                          <div className="card" style={{ width: "18rem" }}>
+                            <img
+                              src={product.imgSrc}
+                              className="card-img-top"
+                              alt="Product Image"
+                            />
+                            <div className="card-body">
+                              <h5 className="card-title">{product.title}</h5>
+                              <p className="card-text">{product.description}</p>
+                              <button className="btn btn-primary mx-3">{product.price}{" "}₹</button>
+                              <button className="btn btn-warning">Add To Cart</button>
+                            </div>
+                          </div>
+                        </div>
+                      </>
+                    );
                 })
             }
           </div>
